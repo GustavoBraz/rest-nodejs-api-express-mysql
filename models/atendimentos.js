9e07a69b7acbe773311cbbdd12a828a62ba6bfe8
Gustavo Braz <gustavo.braz89@gmail.com>
2020-04-02 14:37:31 -0300

    REST-Node-Express-MySQL: Altera status HTTP

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,17 +2,17 @@ const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
-  adiciona(atendimento) {
-    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+  adiciona(atendimento, res) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM');
     const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
     const atendimentoDatado = { ...atendimento, dataCriacao, data };
     const sql = 'INSERT INTO Atendimentos SET ?';
 
     conexao.query(sql, atendimentoDatado, (erro, resultados) => {
       if (erro) {
-        console.log(erro);
+        res.status(400).json(erro);
       } else {
-        console.log(resultados);
+        res.status(201).json(resultados);
       }
     });
   }
